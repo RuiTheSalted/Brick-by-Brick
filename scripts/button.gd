377de32de705971893844f0919686d9374d66bb8
Cristian Ambriz <cristianambrizboi@gmail.com>
2026-02-08 12:42:34 -0800

    Add ball upgrade system with button functionality
    - Added upgrade level tracking to ball
    - Implemented texture switching (ball.png -> iceball.png)
    - Added size and speed increases per upgrade level
    - Connected button to trigger ball upgrades

---
+++ scripts/button.gd
@@ -0,0 +1,22 @@
+extends Button
+
+var ball: CharacterBody2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Find the ball node in the scene
+	ball = get_tree().get_first_node_in_group("ball")
+	
+	if not ball:
+		# Alternative: find ball by unique name in the level
+		var level = get_tree().get_root().get_node("Level")
+		if level:
+			ball = level.get_node("Ball")
+
+func _on_pressed() -> void:
+	print("You are pressing button!")
+	print("Button pressed! Upgrading ball ...")
+	
+	if ball and ball.has_method("upgrade"):
+		ball.upgrade()
+	else:
+		print("Error: Ball not found or upgrade method missing")
